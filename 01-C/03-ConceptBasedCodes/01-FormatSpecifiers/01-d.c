b9f9c388b2fe8b0a6e3727f50e67efc065010a9e
sarveshl07 <sarveshlonkar11@gmail.com>
2022-05-03 16:15:16 -0700

    Adding the Signed Decimal source code

---
+++ 01-C/03-ConceptBasedCodes/01-FormatSpecifiers/01-d.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/*
+// using %d with flags and width
+int main(void)
+{
+	int iNum = 392;
+	char iChar = 'a';
+	//printf("Decimal form of 392 using flags is %+10d.\n", iNum);
+	//printf("Decimal form of 392 using flags is %-10d.\n", iNum);
+	//printf("Decimal form of 392 using flags is %#d.\n", iNum);
+	//printf("Decimal form of 392 using flags is %10d.\n", iNum);
+	return 0;
+}
+
+*/
+
+// using %d 
+///*
+int main(void)
+{
+	int iNum = 392; //999999999;
+	printf("Decimal form of the number 392 is %d.\n", iNum);
+	return 0;
+}
+//*/
